fc694a71e7567e45850e20de1fcacd91af889d44
Dracsis Cella De Cavalli Almeida <dcella-d@c1r4s5.42lisboa.com>
2023-04-11 20:33:24 +0100

    testing new functions

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -3,40 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mstiedl <mstiedl@student.42lisboa.com>     +#+  +:+       +#+        */
+/*   By: dcella-d <dcella-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 09:22:49 by mstiedl           #+#    #+#             */
-/*   Updated: 2022/11/15 18:18:40 by mstiedl          ###   ########.fr       */
+/*   Updated: 2023/04/11 17:40:13 by dcella-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *str1, char const *str2)
+char	*ft_strjoin(char *s1, char *s2, int f)
 {
-	int		len;
-	char	*res;
-	int		i;
-	int		i2;
+	int		u;
+	int		c;
+	char	*s3;
 
-	i = 0;
-	len = (ft_strlen(str1) + ft_strlen(str2));
-	res = ft_calloc(sizeof(char), (len + 1));
-	if (!res)
+	u = ft_strlen(s1) + ft_strlen(s2);
+	if (f == -1)
+		u -= ft_strlen(s1);
+	s3 = (char *)ft_calloc(u + 1, sizeof(char));
+	if (!s3)
 		return (NULL);
-	while (str1[i])
-	{
-		res[i] = str1[i];
-		i++;
-	}
-	i2 = 0;
-	while (str2[i2])
-	{
-		res[i + i2] = str2[i2];
-		i2++;
-	}
-	return (res);
+	c = -1;
+	if (s1 && f != -1)
+		while (s1[++c] && (c < u))
+			s3[c] = s1[c];
+	else
+		c = 0;
+	u = -1;
+	while (s2[++u])
+		s3[c + u] = s2[u];
+	if (f == -2)
+		if (s1)
+			free (s1);
+	return (s3);
 }
+
+// char	*ft_strjoin(char const *str1, char const *str2)
+// {
+// 	int		len;
+// 	char	*res;
+// 	int		i;
+// 	int		i2;
+
+// 	i = 0;
+// 	len = (ft_strlen(str1) + ft_strlen(str2));
+// 	res = ft_calloc(sizeof(char), (len + 1));
+// 	if (!res)
+// 		return (NULL);
+// 	while (str1[i])
+// 	{
+// 		res[i] = str1[i];
+// 		i++;
+// 	}
+// 	i2 = 0;
+// 	while (str2[i2])
+// 	{
+// 		res[i + i2] = str2[i2];
+// 		i2++;
+// 	}
+// 	return (res);
+// }
 
 /* int main()
 {
--- minishell.c
+++ minishell.c
@@ -3,29 +3,88 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mstiedl <mstiedl@student.42lisboa.com>     +#+  +:+       +#+        */
+/*   By: dcella-d <dcella-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 15:28:58 by mstiedl           #+#    #+#             */
-/*   Updated: 2023/04/11 17:02:54 by mstiedl          ###   ########.fr       */
+/*   Updated: 2023/04/11 20:30:36 by dcella-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include "minishell.h"
 
 int main()
 {
-	char *input;
-	
+	int		f;
+	char	*input;
+	char	*hist[1000];
+
+	f = 0;
 	ft_printf("\033[2J\033[1;1H");
 	while(1)
 	{
 		ft_printf("$ ");
 		input = get_next_line(STDIN_FILENO);
+		hist[f] = input;
 		if (ft_strncmp("exit", input, 4) == 0)
 		{
 			free (input);
-			exit(0);
+			exit (0);
 		}
-		free (input);
+		ft_printf("%d\n", searchforquots(input, 34));
 	}
+}
+
+
+// Define a function to handle user input
+void handle_input(char *line) {
+    printf("You entered: %s\n", line);
+    // Add the user's input to the history list
+    add_history(line);
+}
+
+int main(void) {
+    // Set up the readline library
+    rl_bind_key('\t', rl_complete);
+    stifle_history(100);
+
+    // Loop to read user input
+    char *line;
+    while ((line = readline("> ")) != NULL) {
+        // If the user entered a command, handle it
+        handle_input(line);
+        // Free the memory allocated by readline
+        free(line);
+    }
+
+    return 0;
+}
+
+int	searchforquots(char *str, int quote)
+{
+	int	f;
+	int	counter;
+
+	f = 0;
+	counter = 0;
+	while (str[f])
+	{
+		if (str[f] == quote)
+			counter++;
+		f++;
+	}
+	return (counter % 2 != 0);
+}
+
+void	freesplit(char **splited)
+{
+	int	counter;
+
+	counter = 0;
+	while (splited[counter])
+	{
+		free(splited[counter]);
+		counter++;
+	}
+	free (splited);
 }
