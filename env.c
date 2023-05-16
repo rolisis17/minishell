b96d5846bac49dceaf82e5398b0b3ce82eb98440
dcella-d@42lisboa <dracsis_turbo@hotmail.com>
2023-05-16 13:55:49 +0100

    env for valgrind

--- env.c
+++ env.c
@@ -6,7 +6,7 @@
 /*   By: dcella-d <dcella-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 14:42:30 by dcella-d          #+#    #+#             */
-/*   Updated: 2023/05/16 13:30:17 by dcella-d         ###   ########.fr       */
+/*   Updated: 2023/05/16 13:52:05 by dcella-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,13 @@ void	set_path_env(char **envp)
 		return ;
 	else
 	{
+		g_glob.kurva = ft_strjoin(this_folder_is(1), "/minishell");;
 		ft_strlcpy(getenv("_"), g_glob.kurva, ft_strlen(g_glob.kurva) + 1);
+		free (g_glob.kurva);
 		g_glob.kurva = getenv("_");
 	}
+	if (access(g_glob.kurva, F_OK) == -1)
+		g_glob.kurva = NULL;
+	printf("%s\n", g_glob.kurva);
 }
 
